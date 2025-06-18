8210e9a78a77a22fad82f42a1423c2a4a436bd98
Allan Weber <a.cassianoweber@gmail.com>
2025-06-18 19:45:39 +0200

    feat: change serving size

--- frontend/src/components/food-nutrition-facts.tsx
+++ frontend/src/components/food-nutrition-facts.tsx
@@ -1,10 +1,22 @@
 import { Food } from '@/api'
+import { useState } from 'react'
+import { Slider } from './ui/slider'
 
 interface FoodNutritionFactsProps {
   foodData: Food
 }
 
 export function FoodNutritionFacts ({ foodData }: FoodNutritionFactsProps) {
+  const [servingSize, setServingSize] = useState(foodData.servingSizeG)
+
+  const calculateValue = (per100g: number) => {
+    return (per100g * servingSize) / 100
+  }
+
+  const calculateDailyValue = (per100g: number, dailyValue: number) => {
+    return Math.round((calculateValue(per100g) / dailyValue) * 100)
+  }
+
   return (
     <div className='max-w-md mx-auto border border-gray-400 rounded-lg bg-white p-6 shadow nutrition-facts'>
       <h2 className='text-2xl font-bold border-b border-black pb-2 mb-2'>
@@ -14,18 +26,26 @@ export function FoodNutritionFacts ({ foodData }: FoodNutritionFactsProps) {
       <div className='flex items-center mb-2'>
         <input
           type='number'
-          value={Math.round(foodData.servingSizeG)}
+          value={Math.round(servingSize)}
           min={1}
-          className='w-12 border rounded text-center mr-2'
-          readOnly
+          className='w-16 border rounded text-center mr-2'
+          onChange={e => setServingSize(Number(e.target.value))}
         />
         <span>grams of {foodData.name}</span>
       </div>
+      <Slider
+        value={[Math.round(servingSize)]}
+        onValueChange={(value: number[]) => setServingSize(value[0])}
+        min={1}
+        max={500}
+        step={1}
+        className='w-full mb-4 [&_[data-slot=slider-track]]:bg-gray-200'
+      />
       <div className='border-t-4 border-black my-2'></div>
       <div className='flex justify-between items-end mb-2'>
         <span className='text-2xl font-bold'>Calories</span>
         <span className='text-3xl font-bold'>
-          {Math.round((foodData.caloriesPer100g * foodData.servingSizeG) / 100)}
+          {Math.round(calculateValue(foodData.caloriesPer100g))}
         </span>
       </div>
       <div className='text-right text-xs font-semibold mb-2'>
@@ -33,15 +53,9 @@ export function FoodNutritionFacts ({ foodData }: FoodNutritionFactsProps) {
       </div>
       <div className='flex justify-between font-semibold border-t border-gray-300 pt-1'>
         <span>
-          Total Fat{' '}
-          {((foodData.fatsPer100g * foodData.servingSizeG) / 100).toFixed(1)}g
+          Total Fat {calculateValue(foodData.fatsPer100g).toFixed(1)}g
         </span>
-        <span>
-          {Math.round(
-            ((foodData.fatsPer100g * foodData.servingSizeG) / 100 / 65) * 100
-          )}
-          %
-        </span>
+        <span>{calculateDailyValue(foodData.fatsPer100g, 65)}%</span>
       </div>
       <div className='flex justify-between pl-4 text-sm'>
         <span>Saturated Fat (mocked) 4.9g</span>
@@ -62,52 +76,29 @@ export function FoodNutritionFacts ({ foodData }: FoodNutritionFactsProps) {
       </div>
       <div className='flex justify-between font-semibold border-t border-gray-300 pt-1'>
         <span>
-          Sodium{' '}
-          {Math.round((foodData.sodiumPer100g * foodData.servingSizeG) / 100)}mg
+          Sodium {Math.round(calculateValue(foodData.sodiumPer100g))}mg
         </span>
-        <span>
-          {Math.round(
-            ((foodData.sodiumPer100g * foodData.servingSizeG) / 100 / 2300) *
-              100
-          )}
-          %
-        </span>
+        <span>{calculateDailyValue(foodData.sodiumPer100g, 2300)}%</span>
       </div>
       <div className='flex justify-between font-semibold border-t border-gray-300 pt-1'>
         <span>
-          Total Carbohydrates{' '}
-          {((foodData.carbsPer100g * foodData.servingSizeG) / 100).toFixed(1)}g
+          Total Carbohydrates {calculateValue(foodData.carbsPer100g).toFixed(1)}
+          g
         </span>
-        <span>
-          {Math.round(
-            ((foodData.carbsPer100g * foodData.servingSizeG) / 100 / 275) * 100
-          )}
-          %
-        </span>
+        <span>{calculateDailyValue(foodData.carbsPer100g, 275)}%</span>
       </div>
       <div className='flex justify-between pl-4 text-sm'>
         <span>
-          Dietary Fiber{' '}
-          {((foodData.fiberPer100g * foodData.servingSizeG) / 100).toFixed(1)}g
+          Dietary Fiber {calculateValue(foodData.fiberPer100g).toFixed(1)}g
         </span>
-        <span>
-          {Math.round(
-            ((foodData.fiberPer100g * foodData.servingSizeG) / 100 / 28) * 100
-          )}
-          %
-        </span>
+        <span>{calculateDailyValue(foodData.fiberPer100g, 28)}%</span>
       </div>
       <div className='flex justify-between pl-4 text-sm mb-1'>
-        <span>
-          Sugars{' '}
-          {((foodData.sugarPer100g * foodData.servingSizeG) / 100).toFixed(1)}g
-        </span>
+        <span>Sugars {calculateValue(foodData.sugarPer100g).toFixed(1)}g</span>
       </div>
       <div className='flex justify-between font-semibold border-t border-gray-300 pt-1'>
         <span>
-          Protein{' '}
-          {((foodData.proteinPer100g * foodData.servingSizeG) / 100).toFixed(1)}
-          g
+          Protein {calculateValue(foodData.proteinPer100g).toFixed(1)}g
         </span>
       </div>
       <div className='flex justify-between pl-4 text-sm'>
