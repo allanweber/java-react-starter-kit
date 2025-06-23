2a757245b458a4d6dbbcf941a50b96847031c756
Allan Weber <a.cassianoweber@gmail.com>
2025-06-23 18:48:53 +0200

    feat:  show most of the nutrients

--- frontend/src/components/food-nutrition-facts.tsx
+++ frontend/src/components/food-nutrition-facts.tsx
@@ -6,6 +6,17 @@ interface FoodNutritionFactsProps {
   foodData: Food
 }
 
+const SATURATED_FAT_ID = 606
+const TRANS_FAT_ID = 605
+const POLYUNSATURATED_FAT_ID = 646
+const MONOUNSATURATED_FAT_ID = 645
+const CHOLESTEROL_ID = 601
+const VITAMIN_D_ID = 324
+const CALCIUM_ID = 301
+const IRON_ID = 303
+const POTASSIUM_ID = 306
+const CAFFEINE_ID = 262
+
 export function FoodNutritionFacts ({ foodData }: FoodNutritionFactsProps) {
   const [servingSize, setServingSize] = useState(foodData.servingSizeG)
 
@@ -16,6 +27,46 @@ export function FoodNutritionFacts ({ foodData }: FoodNutritionFactsProps) {
   const calculateDailyValue = (per100g: number, dailyValue: number) => {
     return Math.round((calculateValue(per100g) / dailyValue) * 100)
   }
+
+  const saturatedFat = foodData.fullNutrients?.find(
+    nutrient => nutrient.attr_id === SATURATED_FAT_ID
+  )
+
+  const transFat = foodData.fullNutrients?.find(
+    nutrient => nutrient.attr_id === TRANS_FAT_ID
+  )
+
+  const polyunsaturatedFat = foodData.fullNutrients?.find(
+    nutrient => nutrient.attr_id === POLYUNSATURATED_FAT_ID
+  )
+
+  const monounsaturatedFat = foodData.fullNutrients?.find(
+    nutrient => nutrient.attr_id === MONOUNSATURATED_FAT_ID
+  )
+
+  const cholesterol = foodData.fullNutrients?.find(
+    nutrient => nutrient.attr_id === CHOLESTEROL_ID
+  )
+
+  const vitaminD = foodData.fullNutrients?.find(
+    nutrient => nutrient.attr_id === VITAMIN_D_ID
+  )
+
+  const calcium = foodData.fullNutrients?.find(
+    nutrient => nutrient.attr_id === CALCIUM_ID
+  )
+
+  const iron = foodData.fullNutrients?.find(
+    nutrient => nutrient.attr_id === IRON_ID
+  )
+
+  const potassium = foodData.fullNutrients?.find(
+    nutrient => nutrient.attr_id === POTASSIUM_ID
+  )
+
+  const caffeine = foodData.fullNutrients?.find(
+    nutrient => nutrient.attr_id === CAFFEINE_ID
+  )
 
   return (
     <div className='max-w-md mx-auto border border-gray-400 rounded-lg bg-white p-6 shadow nutrition-facts'>
@@ -58,21 +109,37 @@ export function FoodNutritionFacts ({ foodData }: FoodNutritionFactsProps) {
         <span>{calculateDailyValue(foodData.fatsPer100g, 65)}%</span>
       </div>
       <div className='flex justify-between pl-4 text-sm'>
-        <span>Saturated Fat (mocked) 4.9g</span>
-        <span>25%</span>
+        <span>
+          Saturated Fat {calculateValue(saturatedFat?.value ?? 0).toFixed(2)}
+          mg
+        </span>
+        <span>{calculateDailyValue(saturatedFat?.value ?? 0, 20)}%</span>
       </div>
       <div className='flex justify-between pl-4 text-sm'>
-        <span>Trans Fat (mocked) 0g</span>
+        <span>
+          Trans Fat {calculateValue(transFat?.value ?? 0).toFixed(2)}mg
+        </span>
+        <span>{calculateDailyValue(transFat?.value ?? 0, 20)}%</span>
       </div>
       <div className='flex justify-between pl-4 text-sm'>
-        <span>Polyunsaturated Fat (mocked) 1.5g</span>
+        <span>
+          Polyunsaturated Fat{' '}
+          {calculateValue(polyunsaturatedFat?.value ?? 0).toFixed(2)}mg
+        </span>
+        <span>{calculateDailyValue(polyunsaturatedFat?.value ?? 0, 22)}%</span>
       </div>
       <div className='flex justify-between pl-4 text-sm mb-1'>
-        <span>Monounsaturated Fat (mocked) 5.6g</span>
+        <span>
+          Monounsaturated Fat{' '}
+          {calculateValue(monounsaturatedFat?.value ?? 0).toFixed(2)}mg
+        </span>
+        <span>{calculateDailyValue(monounsaturatedFat?.value ?? 0, 300)}%</span>
       </div>
       <div className='flex justify-between font-semibold border-t border-gray-300 pt-1'>
-        <span>Cholesterol (mocked) 104mg</span>
-        <span>35%</span>
+        <span>
+          Cholesterol {calculateValue(cholesterol?.value ?? 0).toFixed(2)}mg
+        </span>
+        <span>{calculateDailyValue(cholesterol?.value ?? 0, 300)}%</span>
       </div>
       <div className='flex justify-between font-semibold border-t border-gray-300 pt-1'>
         <span>
@@ -102,22 +169,30 @@ export function FoodNutritionFacts ({ foodData }: FoodNutritionFactsProps) {
         </span>
       </div>
       <div className='flex justify-between pl-4 text-sm'>
-        <span>Vitamin D (mocked) 0.3mcg</span>
-        <span>2%</span>
+        <span>
+          Vitamin D {calculateValue(vitaminD?.value ?? 0).toFixed(2)}mcg
+        </span>
+        <span>{calculateDailyValue(vitaminD?.value ?? 0, 20)}%</span>
       </div>
       <div className='flex justify-between pl-4 text-sm'>
-        <span>Calcium (mocked) 16mg</span>
+        <span>Calcium {calculateValue(calcium?.value ?? 0).toFixed(2)}mg</span>
+        <span>{calculateDailyValue(calcium?.value ?? 0, 1000)}%</span>
       </div>
       <div className='flex justify-between pl-4 text-sm'>
-        <span>Iron (mocked) 1.8mg</span>
-        <span>10%</span>
+        <span>Iron {calculateValue(iron?.value ?? 0).toFixed(2)}mg</span>
+        <span>{calculateDailyValue(iron?.value ?? 0, 18)}%</span>
       </div>
       <div className='flex justify-between pl-4 text-sm mb-1'>
-        <span>Potassium (mocked) 328.9mg</span>
-        <span>7%</span>
+        <span>
+          Potassium {calculateValue(potassium?.value ?? 0).toFixed(2)}mg
+        </span>
+        <span>{calculateDailyValue(potassium?.value ?? 0, 2600)}%</span>
       </div>
       <div className='flex justify-between font-semibold border-t border-gray-300 pt-1'>
-        <span>Caffeine (mocked) 0mg</span>
+        <span>
+          Caffeine {calculateValue(caffeine?.value ?? 0).toFixed(2)}mg
+        </span>
+        <span>{calculateDailyValue(caffeine?.value ?? 0, 400)}%</span>
       </div>
       <div className='text-xs mt-2 border-t border-gray-300 pt-2'>
         * The % Daily Value (DV) tells you how much a nutrient in a serving of
